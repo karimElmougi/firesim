07cc5749a382905d4939b08511fe5e1fad62efd0
karim <karim.elmougi@gmail.com>
2021-12-27 12:03:38 -0500

    Clean up main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ extern crate lazy_static;
 mod output;
 mod simulation;
 
-use simulation::{Config, Simulation};
+use simulation::Simulation;
 
+use anyhow::{Context, Result};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -17,19 +18,20 @@ struct Opt {
     #[structopt(short, long, default_value = "0")]
     base_year: usize,
 
-    #[structopt(short, long)]
-    config_file: Option<String>,
+    #[structopt(short, long, default_value = "config.toml")]
+    config_file: String,
 }
 
-fn main() {
-    let opt = Opt::from_args();
+fn main() -> Result<()> {
+    let options = Opt::from_args();
 
-    let config = toml::from_str::<Config>(&match opt.config_file {
-        Some(config_file) => std::fs::read_to_string(config_file).expect("can't open config file"),
-        None => std::fs::read_to_string("config.toml").expect("Missing config file"),
-    })
-    .expect("invalid TOML in config file");
+    let config_file_content = std::fs::read_to_string(&options.config_file)
+        .with_context(|| format!("Couldn't open config file `{}`", options.config_file))?;
+
+    let config = toml::from_str(&config_file_content).context("Invalid TOML in config file")?;
 
     let simulation = Simulation::new(config);
-    output::print(simulation, opt.number_of_years, opt.base_year);
+    output::print(simulation, options.number_of_years, options.base_year);
+
+    Ok(())
 }
